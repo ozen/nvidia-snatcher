e93be9bd9ec73cd8c90acb17ee80740c117c36b7
Yigit Ozen <canyigitozen@gmail.com>
2020-09-28 22:26:37 +0300

    Added TR stores

---
+++ src/store/model/itopya.ts
@@ -0,0 +1,71 @@
+import {Store} from './store';
+
+export const Itopya: Store = {
+	labels: {
+		captcha: {
+			container: 'body',
+			text: ['enter the characters you see below']
+		},
+		inStock: {
+			container: '.quantity',
+			text: ['']
+		}
+	},
+	links: [
+		{
+			brand: 'test:brand',
+			model: 'test:model',
+			series: 'test:series',
+			url: 'https://www.incehesap.com/james-donkey-711-siyah-7-1-surround-gaming-kulaklik-fiyati-39979'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3080',
+			url: 'https://www.itopya.com/asus-tuf-geforce-rtx-3080-10gb-gddr6x-320-bit-ekran-karti/'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3080',
+			url: 'https://www.itopya.com/asus-tuf-geforce-rtx-3080-oc-10gb-gddr6x-320-bit-ekran-karti/'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3080',
+			url: 'https://www.itopya.com/asus-rog-strix-geforce-rtx-3080-10gb-gddr6x-320-bit-ekran-karti/'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix oc',
+			series: '3080',
+			url: 'https://www.itopya.com/asus-rog-strix-geforce-rtx-3080-oc-10gb-gddr6x-320-bit-ekran-karti/'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3090',
+			url: 'https://www.itopya.com/asus-tuf-geforce-rtx-3090-24gb-gddr6x-384-bit-ekran-karti/'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.itopya.com/asus-tuf-geforce-rtx-3090-24gb-oc-gddr6x-384-bit-ekran-karti/'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3090',
+			url: 'https://www.itopya.com/asus-rog-strix-geforce-rtx-3090-24gb-gddr6x-384-bit-ekran-karti/'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix oc',
+			series: '3090',
+			url: 'https://www.itopya.com/asus-rog-strix-geforce-rtx-3090-24gb-oc-gddr6x-384-bit-ekran-karti/'
+		}
+	],
+	name: 'itopya'
+};
---
+++ src/store/model/qp.ts
@@ -0,0 +1,71 @@
+import {Store} from './store';
+
+export const QP: Store = {
+	labels: {
+		captcha: {
+			container: 'body',
+			text: ['enter the characters you see below']
+		},
+		inStock: {
+			container: '#product-addtocart-button',
+			text: ['']
+		}
+	},
+	links: [
+		{
+			brand: 'test:brand',
+			model: 'test:model',
+			series: 'test:series',
+			url: 'https://www.qp.com.tr/qp-helios-ryzen-7-3800x-32gb-rtx-2080-ti-500-gb-oyun-bilgisayari'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3080',
+			url: 'https://www.qp.com.tr/asus-rog-strix-geforce-rtx-3080-10gb-320-bit-ekran-karti'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3080',
+			url: 'https://www.qp.com.tr/asus-tuf-gaming-geforce-rtx-3080-10gb-320-bit-ekran-karti'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3090',
+			url: 'https://www.qp.com.tr/asus-tuf-gaming-geforce-rtx-3090-24gb-384-bit-ekran-karti'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3090',
+			url: 'https://www.qp.com.tr/asus-rog-strix-geforce-rtx-3090-24gb-384-bit-ekran-karti'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix oc',
+			series: '3080',
+			url: 'https://www.qp.com.tr/asus-rog-strix-geforce-rtx-3080-oc-edition-10gb-320-bit-ekran-karti'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3080',
+			url: 'https://www.qp.com.tr/asus-tuf-gaming-geforce-rtx-3080-oc-edition-10g-320-bit-ekran-karti'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix oc',
+			series: '3090',
+			url: 'https://www.qp.com.tr/asus-rog-strix-geforce-rtx-3090-oc-edition-24gb-384-bit-ekran-karti'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.qp.com.tr/asus-tuf-gaming-geforce-rtx-3090-oc-edition-24gb-384-bit-ekran-karti'
+		}
+	],
+	name: 'QP'
+};
